978fe4cb87db05c646417e8bafce75dc3e107804
unknown <justshamsulalom@gmail.com>
2020-11-13 14:40:48 +0600

    Ahema Portfolio Website

---
+++ ADMIN/404.php
@@ -0,0 +1,36 @@
+<?php include"inc/header.php"; ?>
+
+<div id="wrapper">
+
+    <?php include"inc/aside.php"; ?>
+
+    <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+
+        <?php include"inc/menu.php"; ?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- 404 Error Text -->
+                    <div class="text-center">
+                        <div class="error mx-auto" data-text="404">404</div>
+                        <p class="lead text-gray-800 mb-5">Page Not Found</p>
+                        <p class="text-gray-500 mb-0">It looks like you found a glitch in the matrix...</p>
+                        <a href="index.html">&larr; Back to Dashboard</a>
+                    </div>
+
+                </div>
+                <!-- /.container-fluid -->
+
+            </div>
+            <!-- End of Main Content -->
+
+             <?php include"inc/footer.php"; ?>
+    </div>
+</div>
+
+
+<?php include"inc/scrool.php"; ?>
+
+<?php include"inc/script.php"; ?>      
---
+++ ADMIN/blank.php
@@ -0,0 +1,32 @@
+<?php include"inc/header.php"; ?>
+
+<div id="wrapper">
+
+    <?php include"inc/aside.php"; ?>
+
+    <div id="content-wrapper" class="d-flex flex-column">
+        <div id="content">
+
+        <?php include"inc/menu.php"; ?>
+
+            <div class="container-fluid">
+                <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+            </div>
+             <!--*****************************                                
+                 ***                       ***
+                 ***Full Page Conatant Here***
+                 ***                       ***
+                 *****************************-->
+            <div class="row">
+                
+            </div>    
+
+        </div>
+        <?php include"inc/footer.php"; ?>
+    </div>
+</div>
+
+
+<?php include"inc/scrool.php"; ?>
+
+<?php include"inc/script.php"; ?> 
